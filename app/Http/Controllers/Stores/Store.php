6c5b79d780493a9591693564fff308c6b0d3907d
sajid9 <muhammadsajid9005@gmail.com>
2019-11-05 17:05:50 +0500

    changes for server

--- app/Http/Controllers/Stores/Store.php
+++ app/Http/Controllers/Stores/Store.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\stores;
 use CH;
-class Store extends Controller
+class store extends Controller
 {
     
     public function store_listing(){
--- routes/web.php
+++ routes/web.php
@@ -412,8 +412,3 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-
-
-
-/* hello changes*/
-
